75fb67135cb49ccb2b09694c96eb1ca53a7dca77
Jonathan Lepolt <lepoltj@seapine.com>
2013-01-18 11:41:03 -0500

    Update build file to include 'fullpage' plugin

--- tools/build_tinymce_sproutcore.py
+++ tools/build_tinymce_sproutcore.py
@@ -10,7 +10,7 @@ DEST = None
 THEME = 'advanced'
 SKIN = 'default'
 LANG = 'en'
-PLUGINS = ('paste', 'table', 'autoresize', 'seapine', 'sproutcore', 'lists')
+PLUGINS = ('paste', 'table', 'autoresize', 'seapine', 'sproutcore', 'lists', 'fullpage')
 
 BASEPATH = os.path.join('..', 'jscripts', 'tiny_mce')
 TINYMCE_SRC_NAME = 'tiny_mce_jquery_src.js' if DEBUG else 'tiny_mce_jquery.js'
