5c38c10ed0a72078996c6a2b7911e9889f916b69
Jonathan Lepolt <lepoltj@seapine.com>
2014-01-13 12:00:06 -0500

    Add ability to created un-minified bundle with jquery

--- Jakefile.js
+++ Jakefile.js
@@ -213,7 +213,7 @@ task("bundle", ["minify"], function(params) {
 });
 
 desc("Bundles in plugins/themes without minifying into a tinymce.full.js file");
-task("dev-bundle", function (params) {
+task("bundle-full", ["default"], function (params) {
   var inputFiles, fullContent, addPlugins = true;
 
   function appendAddon (name) {
@@ -255,6 +255,51 @@ task("dev-bundle", function (params) {
   }
 
   fs.writeFileSync("js/tinymce/tinymce.full.js", fullContent);
+});
+
+desc("Bundles in the plugins/themes without minifying, including jQuery into a tinymce.jquery.full.js file");
+task("bundle-full-jquery", ["default"], function (params) {
+	var inputFiles, fullContent, addPlugins = true;
+
+	function appendAddon (name) {
+		if (addPlugins) {
+			if (name == '*') {
+				glob.sync('js/tinymce/plugins/*/plugin.js').forEach(function (filePath) {
+					fullContent += "\n;" + fs.readFileSync(filePath).toString();
+				});
+			} else {
+				fullContent += "\n;" + fs.readFileSync("js/tinymce/plugins/" + name + "/plugin.js").toString();
+			}
+		} else {
+			if (name == '*') {
+				glob.sync('js/tinymce/themes/*/theme.min.js').forEach(function (filePath) {
+					fullContent += "\n;" + fs.readFileSync(filePath).toString();
+				});
+			} else {
+				fullContent += "\n;" + fs.readFileSync("js/tinymce/themes/" + name + "/theme.js").toString();
+			}
+		}
+	}
+
+	fullContent = fs.readFileSync("js/tinymce/tinymce.jquery.js").toString();
+
+	if (arguments[0] == '*') {
+		arguments = ['themes:*', 'plugins:*'];
+	}
+
+	for (var i = 0; i < arguments.length; i++) {
+		var args = arguments[i].split(':');
+
+		if (args[0] == 'plugins') {
+			addPlugins = true;
+		} else if (args[0] == 'themes') {
+			addPlugins = false;
+		}
+
+		appendAddon(args[1] || args[0]);
+	}
+
+	fs.writeFileSync("js/tinymce/tinymce.jquery.full.js", fullContent);
 });
 
 desc("Runs JSHint on core source files");
@@ -350,6 +395,7 @@ task("zip-production", [], function () {
 		exclude: [
 			"js/tinymce/tinymce.js",
 			"js/tinymce/tinymce.dev.js",
+      "js/tinymce/tinymce.full.js",
 			"js/tinymce/tinymce.full.min.js",
 			"js/tinymce/tinymce.jquery.js",
 			"js/tinymce/tinymce.jquery.min.js",
@@ -396,6 +442,7 @@ task("zip-production-jquery", [], function () {
 			"js/tinymce/tinymce.js",
 			"js/tinymce/tinymce.min.js",
 			"js/tinymce/tinymce.dev.js",
+      "js/tinymce/tinymce.full.js",
 			"js/tinymce/tinymce.full.min.js",
 			"js/tinymce/tinymce.jquery.js",
 			"js/tinymce/tinymce.jquery.dev.js",
